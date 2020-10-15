b5dfa574913e9d2c73a250523d0faa6fab9af4e2
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-10-15 14:29:17 +0100

    Add initial project files

---
+++ app/src/main/java/com/ml/chucknorrisapp/view/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ml.chucknorrisapp.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ml.chucknorrisapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
