6d826ced642897e7ca8102c7b9dace4f9264158c
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-10-16 12:46:24 +0100

    Register DisplayJokeFragment and SearchJokeFragment classes to HomeActivity class

--- app/src/main/java/com/ml/chucknorrisapp/view/activity/HomeActivity.kt
+++ app/src/main/java/com/ml/chucknorrisapp/view/activity/HomeActivity.kt
@@ -2,11 +2,30 @@ package com.ml.chucknorrisapp.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Display
+import androidx.fragment.app.Fragment
 import com.ml.chucknorrisapp.R
+import com.ml.chucknorrisapp.view.fragment.DisplayJokeFragment
+import com.ml.chucknorrisapp.view.fragment.SearchJokeFragment
 
+/**
+ * Class is used as the entry point for the application
+ */
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        loadFragments()
+    }
+
+    /**
+     * Function loads SearchJokeFragment and DisplayJokeFragment into FrameLayouts
+     */
+    private fun loadFragments(){
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.search_frame, SearchJokeFragment())
+            .add(R.id.display_frame, DisplayJokeFragment())
+            .commit()
     }
 }
