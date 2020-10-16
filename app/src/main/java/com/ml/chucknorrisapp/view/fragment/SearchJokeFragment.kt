7af0ff4ccbb0c38b1bce24039c7033ad021cf813
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-10-16 13:23:07 +0100

    Initialise search views

--- app/src/main/java/com/ml/chucknorrisapp/view/fragment/SearchJokeFragment.kt
+++ app/src/main/java/com/ml/chucknorrisapp/view/fragment/SearchJokeFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import com.ml.chucknorrisapp.R
 import com.ml.chucknorrisapp.databinding.FragmentSearchJokeBinding
 import com.ml.chucknorrisapp.viewmodel.JokeViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -15,6 +18,8 @@ import org.koin.android.viewmodel.ext.android.viewModel
 class SearchJokeFragment : Fragment() {
 
     private val jokeViewModel:JokeViewModel by viewModel()
+    lateinit var searchEditText: EditText
+    lateinit var searchButton: Button
 
     /**
      * Function is used to bind the layout file to application componetns
@@ -37,13 +42,22 @@ class SearchJokeFragment : Fragment() {
      */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initSearchViews(view)
+    }
 
+    /**
+     * Initialise Search Views
+     * @param view
+     */
+    private fun initSearchViews(view: View){
+        searchEditText = view.findViewById(R.id.joke_search_edittext)
+        searchButton = view.findViewById(R.id.search_button)
     }
 
     /**
      * Function handles the search input and passes it to the JokeViewModel
      */
     fun handleSearchInput(){
-        //TODO implement search functionality on ViewModel, Repository and Remote Data Source
+
     }
 }
