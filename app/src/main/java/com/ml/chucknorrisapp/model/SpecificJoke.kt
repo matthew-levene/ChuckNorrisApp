6f73ae10dac0cc2e3b5ebde7e5d30e0af8efaf3e
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-10-16 15:52:13 +0100

    Add KDocs to the SpecificJoke class

--- app/src/main/java/com/ml/chucknorrisapp/model/SpecificJoke.kt
+++ app/src/main/java/com/ml/chucknorrisapp/model/SpecificJoke.kt
@@ -2,6 +2,11 @@ package com.ml.chucknorrisapp.model
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Class is used as a placeholder when retrieve specific jokes from the API
+ * @constructor takes three parameters:
+ * - A Joke instance
+ */
 data class SpecificJoke(
     @SerializedName("value") val joke: Joke
 )
