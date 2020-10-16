53189700f4ef2c95b4a226680cb802423618662e
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-10-16 09:20:45 +0100

    FIX: Register Converters class to JokeDatabase class

--- app/src/main/java/com/ml/chucknorrisapp/model/db/JokeDatabase.kt
+++ app/src/main/java/com/ml/chucknorrisapp/model/db/JokeDatabase.kt
@@ -2,6 +2,7 @@ package com.ml.chucknorrisapp.model.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.ml.chucknorrisapp.model.JokeResponse
 import com.ml.chucknorrisapp.model.db.DatabaseConstants.DATABASE_VERSION
 import com.ml.chucknorrisapp.model.db.DatabaseConstants.EXPORT_SCHEME_OPTION
@@ -10,6 +11,7 @@ import com.ml.chucknorrisapp.model.db.DatabaseConstants.EXPORT_SCHEME_OPTION
  * Class is used to store JokeResponse objects locally
  */
 @Database(entities = [JokeResponse::class], version = DATABASE_VERSION, exportSchema = EXPORT_SCHEME_OPTION)
+@TypeConverters(Converters::class)
 abstract class JokeDatabase : RoomDatabase() {
     abstract val jokeDao: JokeDao
 }
