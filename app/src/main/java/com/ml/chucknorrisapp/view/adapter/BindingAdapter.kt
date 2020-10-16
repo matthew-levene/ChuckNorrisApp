8d084db714cd767794b4edad61d4f53a8e192341
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-10-16 13:10:48 +0100

    Add Chuck Norris Image to Row Joke Item Layout

---
+++ app/src/main/java/com/ml/chucknorrisapp/view/adapter/BindingAdapter.kt
@@ -0,0 +1,15 @@
+package com.ml.chucknorrisapp.view.adapter
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.ml.chucknorrisapp.model.network.NetworkConstants.CHUCK_NORRIS_IMAGE
+
+@BindingAdapter("loadImage")
+fun loadChuckNorrisImage(imageView: ImageView, url: String){
+    Glide
+        .with(imageView.context)
+        .load(CHUCK_NORRIS_IMAGE)
+        .centerCrop()
+        .into(imageView)
+}
