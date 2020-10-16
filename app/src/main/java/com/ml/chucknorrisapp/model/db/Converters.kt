0d1f08b08566a464f091c6e952b0ec8710f9a0e6
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-10-16 18:09:42 +0100

    Update Converters class

--- app/src/main/java/com/ml/chucknorrisapp/model/db/Converters.kt
+++ app/src/main/java/com/ml/chucknorrisapp/model/db/Converters.kt
@@ -17,7 +17,7 @@ class Converters {
      * @return String representation of the list
      */
     @TypeConverter
-    fun jokeListToJson(jokes: List<Joke>) : String{
+    fun jokeListToJson(jokes: List<Joke>) : String {
         return Gson().toJson(jokes)
     }
 
@@ -30,4 +30,27 @@ class Converters {
     fun jokeListFromJson(value: String): List<Joke> {
         return Gson().fromJson(value, Array<Joke>::class.java).toList()
     }
+
+    /**
+     * Function converts list of String objects to String
+     * @param categories
+     * @return String representation of the list
+     */
+
+    @TypeConverter
+    fun stringListToJson(categories: List<String>) : String {
+        return Gson().toJson(categories)
+    }
+
+    /**
+     * Function takes a parameter of string and retrieves a list
+     * @param value
+     * @return List of String objects
+     */
+    @TypeConverter
+    fun stringListFromJson(value: String): List<String> {
+        return Gson().fromJson(value, Array<String>::class.java).toList()
+    }
+
+
 }
