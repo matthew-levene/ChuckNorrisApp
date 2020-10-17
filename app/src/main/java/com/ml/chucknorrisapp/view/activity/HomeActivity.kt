7fe19ce5b15f946b692ef85fb2ac7b507fec0515
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-10-17 11:11:35 +0100

    Implement BottomNavigationView

--- app/src/main/java/com/ml/chucknorrisapp/view/activity/HomeActivity.kt
+++ app/src/main/java/com/ml/chucknorrisapp/view/activity/HomeActivity.kt
@@ -3,7 +3,9 @@ package com.ml.chucknorrisapp.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Display
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ml.chucknorrisapp.R
 import com.ml.chucknorrisapp.view.fragment.DisplayJokeFragment
 import com.ml.chucknorrisapp.view.fragment.SearchJokeFragment
@@ -12,10 +14,27 @@ import com.ml.chucknorrisapp.view.fragment.SearchJokeFragment
  * Class is used as the entry point for the application
  */
 class HomeActivity : AppCompatActivity() {
+
+    /**
+     * Declare an item selected listener to listen for click events on the Bottom Navigation View
+     */
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when(item.itemId){
+            R.id.nav_home -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_favourites -> {
+
+                return@OnNavigationItemSelectedListener true }
+        }
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         loadFragments()
+        loadBottomNavigation()
     }
 
     /**
@@ -28,4 +47,14 @@ class HomeActivity : AppCompatActivity() {
             .add(R.id.display_frame, DisplayJokeFragment())
             .commit()
     }
+
+    /**
+     * Function loads the Bottom Navigation Bar
+     */
+    private fun loadBottomNavigation(){
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+        bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+
 }
