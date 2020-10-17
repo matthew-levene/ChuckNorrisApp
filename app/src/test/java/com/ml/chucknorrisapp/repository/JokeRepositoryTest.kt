c8b3c5311429c72b42d20a2762808085f279636f
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-10-17 21:36:51 +0100

    Write Mockito test cases for the JokeRepository class

---
+++ app/src/test/java/com/ml/chucknorrisapp/repository/JokeRepositoryTest.kt
@@ -0,0 +1,87 @@
+package com.ml.chucknorrisapp.repository
+
+import com.ml.chucknorrisapp.model.Joke
+import com.ml.chucknorrisapp.model.JokeResponse
+import com.ml.chucknorrisapp.model.SpecificJoke
+import com.ml.chucknorrisapp.model.db.JokeDao
+import com.ml.chucknorrisapp.model.network.JokeApiService
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.koin.java.KoinJavaComponent.inject
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+
+
+class JokeRepositoryTest  {
+
+    private lateinit var jokeDao:JokeDao
+    private lateinit var jokeApiService: JokeApiService
+
+    private lateinit var jokeRepository:JokeRepository
+
+    private lateinit var jokeResponse:JokeResponse
+
+    @Before
+    fun setUp() {
+        jokeDao = mock(JokeDao::class.java)
+        jokeApiService = mock(JokeApiService::class.java)
+
+        jokeRepository = JokeRepository(jokeDao, jokeApiService)
+
+        val jokesList:List<Joke> = listOf(
+            Joke(1, "Hello World", emptyList()),
+            Joke(20, "Testing", listOf("Nerdy", "Explicit"))
+        )
+
+        jokeResponse = JokeResponse(0,1, jokesList)
+    }
+
+    @Test
+    fun test_getJokes(): Unit = runBlocking {
+
+        val deferredResponse = mock(Deferred::class.java)
+        `when`(jokeApiService.getRandomJokes()).thenReturn(deferredResponse as Deferred<JokeResponse>?)
+        //doNothing().`when`(jokeDao).insertJokes(jokeResponse)
+
+        jokeRepository.getJokes()
+
+        verify(jokeApiService, times(1)).getRandomJokes()
+        //verify(jokeDao, times(1)).insertJokes(jokeResponse)
+    }
+
+    @Test
+    fun test_getSpecificJoke() : Unit = runBlocking {
+        val deferredResponse = mock(Deferred::class.java)
+        `when`(jokeApiService.getSpecificJoke(5)).thenReturn(deferredResponse as Deferred<SpecificJoke>?)
+        //doNothing().`when`(jokeDao).insertJokes(jokeResponse)
+
+        jokeRepository.getSpecificJoke(5)
+
+        verify(jokeApiService, times(1)).getSpecificJoke(5)
+        //verify(jokeDao, times(1)).insertJokes(jokeResponse)
+    }
+
+    @Test
+    fun test_insertFavouriteJoke() {
+        doNothing().`when`(jokeDao).insertFavouriteJoke(Joke(1,"Testing", emptyList()))
+
+        jokeRepository.insertFavouriteJoke(Joke(1,"Testing", emptyList()))
+
+        verify(jokeDao, times(1)).insertFavouriteJoke(Joke(1,"Testing", emptyList()))
+    }
+
+    @Test
+    fun test_deleteFavouriteJoke() {
+        doNothing().`when`(jokeDao).deleteFavouriteJoke(Joke(1,"Testing", emptyList()))
+
+        jokeRepository.deleteFavouriteJoke(Joke(1,"Testing", emptyList()))
+
+        verify(jokeDao, times(1)).deleteFavouriteJoke(Joke(1,"Testing", emptyList()))
+    }
+}
