3031a0e1b9e82710333c22bce200deece5f0c2b3
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-10-20 09:10:48 +0100

    Add JavaDoc

--- app/src/main/java/com/ml/chucknorrisapp/di/JokeViewModelModule.kt
+++ app/src/main/java/com/ml/chucknorrisapp/di/JokeViewModelModule.kt
@@ -5,6 +5,11 @@ import com.ml.chucknorrisapp.viewmodel.JokeViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
+/**
+ * Module is used by Koin to provide a JokeViewModel and
+ * FavouriteJokeViewModel instance to the application
+ */
+
 val viewModelModule = module {
    viewModel { JokeViewModel(get()) }
    viewModel { FavouriteJokeViewModel(get()) }
